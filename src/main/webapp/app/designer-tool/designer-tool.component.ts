833ae8c03fc38328bf00f03e95d15b5352bb31d6
ismail.mansouri <ismail.mansouri@rhis-solutions.com>
2024-01-27 15:01:45 +0100

    finelizing mercher login

---
+++ src/main/webapp/app/designer-tool/designer-tool.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-designer-tool',
+  templateUrl: './designer-tool.component.html',
+  styleUrls: ['./designer-tool.component.scss'],
+})
+export class DesignerToolComponent {}
